8e5c3eebb7ec3460c30b2f9d9e63dbdf729cf4cf
Nikotinsv <nikandr97@mail.ru>
2022-01-16 15:04:13 +0300

    Task 3 bugFix

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -5,13 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Subsequence {
-    public static void main(String[] args) {
-        Subsequence s = new Subsequence();
-        boolean b = s.find(Arrays.asList("A", "B", "C", "D"),
-                Arrays.asList("BD", "A", "ABC", "B", "M", "D", "M", "C", "DC", "D"));
-        System.out.println(b); // Result: true
-    }
-
     /**
      * Checks if it is possible to get a sequence which is equal to the first
      * one by removing some elements from the second one.
@@ -20,25 +13,26 @@ public class Subsequence {
      * @param y second sequence
      * @return <code>true</code> if possible, otherwise <code>false</code>
      */
-
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
         // TODO: Implement the logic here
-        try {
-            List temp = new ArrayList();
-            int j = 0;
-            for (Object o : x) {
-                for (; j < y.size(); j++) {
-                    if (o.equals(y.get(j))) {
-                        temp.add(y.get(j));
-                        j++;
-                        break;
-                    }
-                }
-            }
-            return x.equals(temp);
-        } catch (IllegalArgumentException | NullPointerException e) {
+        if (x == null || y == null) {
+            throw new IllegalArgumentException();
+        }
+        if (x.size() > y.size()) {
             return false;
         }
+        List temp = new ArrayList();
+        int j = 0;
+        for (Object o : x) {
+            for (; j < y.size(); j++) {
+                if (o.equals(y.get(j))) {
+                    temp.add(y.get(j));
+                    j++;
+                    break;
+                }
+            }
+        }
+        return x.equals(temp);
     }
 }
